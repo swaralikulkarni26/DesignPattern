f2bcb730b8f5c5968c9c9a971d8aac6e1822f59d
swaralikulkarni26 <94820625+swaralikulkarni26@users.noreply.github.com>
2024-02-09 15:21:54 +0530

    singleton Design patter added

---
+++ SingletonDesignPattern/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = Logger.getInstance();
+            logger.Log("This is first Logger Instance");
+            logger.Log("This is second instance");
+        }
+    }
+}
