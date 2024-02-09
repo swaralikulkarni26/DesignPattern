a9da23d9eec6c3dfc3b1667976dad193baab9bd4
swaralikulkarni26 <94820625+swaralikulkarni26@users.noreply.github.com>
2024-02-09 15:23:33 +0530

    added in Dsign patter folder

---
+++ DesignPattern/SingletonDesignPattern/ConsoleApp1/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+   public class Logger
+    {
+        private static Logger instance;
+        private  Logger()
+        {
+
+        }
+
+       public static Logger getInstance()
+        {
+           
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+
+            }
+            return instance;
+        }
+
+       public void Log(string msg)
+        {
+            Console.WriteLine($"{ DateTime.Now} { msg}");
+        }
+           
+
+    }
+}
